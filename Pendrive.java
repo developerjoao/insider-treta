0e87a7f3a6435922b400fc27c6688e3f9bac03c3
Joao <jgazbral@gmail.com>
2018-11-25 18:31:13 +0000

    classes das atividades do usuario

---
+++ Pendrive.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author jvfca
+ *
+ */
+public class Pendrive extends Device{
+	private boolean insert;
+
+	public boolean isInsert() {
+		return insert;
+	}
+
+	public void setInsert(boolean insert) {
+		this.insert = insert;
+	}
+}
