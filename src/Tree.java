53378f57e7e29ba271978c20119cb0e9a9ee1ae1
Joao <jgazbral@gmail.com>
2018-11-28 21:50:45 +0000

    adicionada estrutura de arvore para adicionar as atividades de device

--- src/Tree.java
+++ src/Tree.java
@@ -1,16 +1,36 @@
 public class Tree {
     private Node root;
 
-    public Tree() {
+    public Tree(String user) {
+        Node<User> usuario = new Node<User>(user);
+        this.root = user;
 
     }
-
+    /***
     public void addUser(Node<User> user) {
         this.root = user;
-    }
+    }*/
 
     public void addDate(Node<TimeWindow> timeWindow) {
 
+    }
+
+    public void addHTTP(String url){
+        Node<String> http = new Node<String>("Http");
+        http.addChildren(url)
+        this.root.addChildren(http);
+    }
+
+    public void addPendrive(String insert){
+        Node<String> pendrive = new Node<String>("Pendrive");
+        pendrive.addChildren(insert)
+        this.root.addChildren(pendrive);
+    }
+
+    public void addLogon(String logon){
+        Node<String> computer = new Node<String>("Computer");
+        computer.addChildren(logon)
+        this.root.addChildren(computer);
     }
 
     public void showTree() {
