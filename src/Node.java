5c28e517c756b1192ab3be54bfc4ffcc83bcd7a1
Joao <jgazbral@gmail.com>
2018-11-29 03:25:23 +0000

    Adição de comentarios javadoc

--- src/Node.java
+++ src/Node.java
@@ -5,12 +5,20 @@ public class Node<T> {
     private int[] histogram;
     private ArrayList<Node<String>> children;
 
+    
+    /***
+     * @param value O valor que inicia o nó.
+     * */
     public Node(T value) {
         this.value = value;
         this.histogram = new int[24];
         this.children = new ArrayList<Node<String>>();
     }
-
+    /***
+     * Retorna os filhos de um nó para poder manipulá-los
+     * no processo de leitura dos CSV.
+     *@return Os filhos do nó em questão.
+     * */
     public ArrayList<Node<String>> getChildren(){
     	return this.children;
     }
@@ -19,10 +27,18 @@ public class Node<T> {
 
     }
 
+    /***
+     * Adiciona um filho no array de filhos do nó.
+     * @param newChildren o novo filho a ser adicionado ao nó em questão.
+     * */
     public void addChildren(Node<String> newChildren){
         this.children.add(newChildren);
     }
-
+    
+    /***
+     * Retorna o valor de um nó específico.
+     * @return O valor do nó em questão.
+     * */
     public T getValue() {
         return this.value;
     }
